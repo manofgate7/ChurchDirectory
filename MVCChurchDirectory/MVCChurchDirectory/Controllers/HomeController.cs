d2b4c28df3493e902ce4a85c89bea8507b44fee5
manofgate7 <manofgate@gmail.com>
2018-06-18 17:50:52 -0600

    add in the database

--- MVCChurchDirectory/MVCChurchDirectory/Controllers/HomeController.cs
+++ MVCChurchDirectory/MVCChurchDirectory/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCChurchDirectory.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,16 @@ namespace MVCChurchDirectory.Controllers
 {
     public class HomeController : Controller
     {
+        static string connString = "Data Source=(LocalDB)\\v12.0; " +
+            "Integrated Security=SSPI;" +
+            "AttachDbFileName=|DataDirectory|\\ChurchDir.MDF;";
+        private ChurchDBContext dtb = new ChurchDBContext(connString);
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
---
+++ MVCChurchDirectory/MVCChurchDirectory/Models/ChurchDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCChurchDirectory.Models
+{
+    public class ChurchDBContext : DbContext
+    {
+        public ChurchDBContext(string connString)
+        {
+            this.Database.Connection.ConnectionString = connString;
+        }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Child> Children { get; set; }
+
+    }
+}
